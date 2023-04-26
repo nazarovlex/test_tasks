a990b50e66c2223a5b8ed424b94b36d907b9c639
nazarovlex <collermix@gmail.com>
2023-04-26 17:12:35 +0300

    Changed the "possible_numbers" class from list to str, changed the algorithm and added an exception handler to the "find_right_sum" function, changed the comment language to Russian.

--- task_1.py
+++ task_1.py
@@ -1,35 +1,40 @@
-class Solution():
-    def find_right_sum(self, n, m):
-        # In correct answer n can't be bigger then m
+class Solution:
+    def find_right_sum(self, n: int, m: int) -> list[str]:
+        # Елси n>m то правильных вариантов быть не может, поэтому сразу возвращается пустой массив
         if n > m:
             return []
 
-        # Array of possible numbers in combination
-        possible_numbers = [x for x in range(1, n + 1)]
+        # Строка из всех доступных для комбинации чисел
+        possible_numbers = "".join(str(x) for x in range(1, n + 1))
 
-        # Result array
+        # Массив со всеми правильными результатами
         res = []
-
-        for i in range(1, len(possible_numbers)):
-            for j in range(i + 1, len(possible_numbers) + 1):
-                # first number
-                start = "".join(str(possible_numbers[x]) for x in range(0, i))
-                # middle slice with "+" between numbers
-                middle = "+".join(str(possible_numbers[x]) for x in range(i, j))
-                # the last number
-                end = "".join(str(possible_numbers[x]) for x in range(j, n))
-
-                # Combination of numbers
-                combination = start + '+' + middle + end
-
-                # If sum of combination == m , write down it  to res
+        # i проходится по номерам всех возможных комбинаций, начиная с 0
+        for i in range(2 ** (len(possible_numbers) - 1)):
+            # начинается комбинация всегда с 1
+            combination = str(possible_numbers[0])
+            # j проходится по индексам всей строки кроме '1', которая уже записана в комбо
+            for j in range(1, len(possible_numbers)):
+                # i преобразуется в двоичную систему и проверяется число на каждой позиции в двоичном представлении i
+                # если на позиции 0, то "+" не ставится перед тем как добавить следующее число в комбо,
+                # а если 1, то "+" ставится перед тем как добавить следующее число в комбо
+                if (i >> (j - 1)) & 1:
+                    combination += "+" + str(possible_numbers[j])
+                else:
+                    combination += str(possible_numbers[j])
+            # обработчик исключений, который ловит ошибку когда создается комбинация, содержащая "+0[1-9]"
+            # так как данная комбинация неправильная просто пропускаем ее
+            try:
                 if eval(combination) == m:
                     res.append(combination)
-        # Return correct results
+            except SyntaxError:
+                continue
+
+        # Возвращаем все правильные варианты в массиве
         return res
 
 
-# Check correct input
+# Проверка правильности ввода n и m
 while True:
     try:
         n, m = map(int, input().split())
@@ -38,9 +43,8 @@ while True:
             for num in result:
                 print(num + "=" + str(m))
         else:
-            print("None")
+            print("Нет правильных вариантов")
     except ValueError:
-        print("Enter two integers separated by a space")
+        print("Введите два натуральных числа через пробел")
         continue
     break
-
